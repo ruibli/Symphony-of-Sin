4ab0732632bbd8a85c7595fdc6481f22a285e3db
ruibli <144478523+ruibli@users.noreply.github.com>
2024-02-09 10:03:32 -0500

    fixed room min and max

--- dungeon_generation.gd
+++ dungeon_generation.gd
@@ -2,8 +2,8 @@ extends Node
 
 var room = preload("res://Room.tscn")
 
-var min_number_rooms = 6
-var max_number_rooms = 8
+var min_number_rooms = 10
+var max_number_rooms = 20
 
 var generation_chance = 20
 
