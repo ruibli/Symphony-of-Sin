5b5ffe6b151213d20d7930ee7d5b2a7870c0615a
CasoWaso <caso1551@gmail.com>
2024-02-22 12:12:46 -0500

    nova animations and arrow

---
+++ assets/entities/nova/arrow.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+@export var speed = -250
+
+func start(pos):
+	$Arrow.position = pos
+
+func _process(delta):
+	$Arrow.position.y -= speed * delta
+
+func _on_area_entered(area):
+	if area.is_in_group("enemies"):
+		area.explode()
+		queue_free()
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
