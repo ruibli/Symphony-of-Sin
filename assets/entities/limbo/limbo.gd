fb35ba87e11d3a7e59230e6b75155a887ecd631e
CasoWaso <caso1551@gmail.com>
2024-02-27 12:15:39 -0500

    Update limbo with movement

--- assets/entities/limbo/limbo.gd
+++ assets/entities/limbo/limbo.gd
@@ -1,47 +1,56 @@
-extends RigidBody2D
+### Enemy.gd
 
-var speed = 200
+extends CharacterBody2D
+
+# Node refs
+@onready var player = get_tree().root.get_node("sos/nova")
+
+# Enemy stats
+@export var speed = 50
+var direction : Vector2 # current direction
+var new_direction = Vector2(0,1) # next direction
+
+# Direction timer
+var rng = RandomNumberGenerator.new()
+var timer = 0
 
 func _ready():
-	var mob_types = $AnimatedSprite2D.sprite_frames.get_animation_names()
-	$AnimatedSprite2D.play(mob_types[randi() % mob_types.size()])
+	rng.randomize()
 
-func _process(delta):
-	# change to momement ai
-	var velocity = Vector2.ZERO 
+func _on_timer_timeout():
+# distance of enemy to player
+	var player_distance = player.position - position
+	#turn to player if in its radius
+	if player_distance.length() <= 20:
+		new_direction = player_distance.normalized()
+	#move toward player
+	elif player_distance.length() <= 100 and timer == 0:
+		direction = player_distance.normalized()
+	#random roam
+	elif timer == 0:
+	#random direction
+		var random_direction = rng.randf()
+		if random_direction < 0.05:
+			#enemy stops
+			direction = Vector2.ZERO
+		elif random_direction < 0.1:
+			#enemy moves
+			direction = Vector2.DOWN.rotated(rng.randf() * 2 * PI)
 
-	if velocity.length() >= 0:
-		velocity = velocity.normalized() * speed
-		$LimboBodyAnim.play()
-		$LimboArmAnim.play()
+func _physics_process(delta):
+	var movement = speed * direction * delta
+	var collision = move_and_collide(movement)
+
+	#if the enemy collides with other objects, turn them around and re-randomize the timer countdown
+	if collision != null and collision.get_collider().name != "Player":
+		#direction rotation
+		direction = direction.rotated(rng.randf_range(PI/4, PI/2))
+		#timer countdown random range
+		timer = rng.randf_range(2, 5)
+	#if they collide with the player trigger the timer's timeout() so that they can move toward the player
 	else:
-		$LimboBodyAnim.stop()
-		$LimboArmAnim.stop()
-	
-	$LimboBodyAnim.position += velocity * delta
-	$LimboArmAnim.position += velocity * delta
+		timer = 0
 
-	# Animations
-	if velocity.x < 0:
-		$LimboBodyAnim.animation = "walk_left"
-		$LimboBodyAnim.flip_h = false
-		$LimboArmAnim.animation = "walk_left"
-		$LimboArmAnim.flip_h = false
-	elif velocity.x > 0:
-		$LimboBodyAnim.animation = "walk_left" # right
-		$LimboBodyAnim.flip_h = true
-		$LimboArmAnim.animation = "walk_left"
-		$LimboArmAnim.flip_h = true
-	elif velocity.y < 0:
-		$LimboBodyAnim.animation = "walk_up"
-		$LimboBodyAnim.flip_h = false
-		$LimboArmAnim.animation = "walk_up"
-		$LimboArmAnim.flip_h = false
-	elif velocity.y > 0:
-		$LimboBodyAnim.animation = "walk_down"
-		$LimboBodyAnim.flip_h = false
-		$LimboArmAnim.animation = "walk_down"
-		$LimboArmAnim.flip_h = false
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
