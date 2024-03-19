af1f38933fd88ce53d3a5fe753be34bffb76faaf
ruibli <144478523+ruibli@users.noreply.github.com>
2024-03-19 09:48:09 -0400

    limbo move ai done

--- assets/entities/limbo/limbo.gd
+++ assets/entities/limbo/limbo.gd
@@ -2,9 +2,6 @@
 
 extends CharacterBody2D
 
-# Node refs
-@onready var player
-
 # Enemy stats
 var mod = 1 + 0.1 * (Glova.g_mod())
 var speed = 125 * mod
@@ -15,18 +12,31 @@ var attack = 1
 var active = false
 var wait = false
 var distance = 0
+var see = false
 
 var can_hit = true
 var direction = "down"
 var type = "move"
 
+func sight():
+	var space_state = get_world_2d().direct_space_state
+	var query = PhysicsRayQueryParameters2D.create(global_position, Glova.g_pos())
+	var sight_check = space_state.intersect_ray(query)
+	if sight_check:
+		if sight_check.collider.name == "nova":
+			return true
+		else:
+			return false
+
 func _physics_process(_delta):
 	if active:
 		distance = global_position.distance_to(Glova.g_pos())
+		see = sight()
+		
 		if not wait:
 			await get_tree().create_timer(0.25).timeout
 			wait = true
-		elif (distance >= 24):
+		elif (distance >= 24 or not see):
 			$NavigationAgent2D.set_target_position(Glova.g_pos())
 			var current_agent_position = global_position
 			var next_path_position = $NavigationAgent2D.get_next_path_position()
@@ -73,7 +83,6 @@ func _on_limbohit_hurt_area_entered(area):
 		area.hit(damage)
 
 func _on_navigation_agent_2d_velocity_computed(safe_velocity):
-	print("test")
-	if active and (distance >= 24):
+	if active and (distance >= 24 or not see):
 		velocity = safe_velocity.normalized() * speed
 		move_and_slide()
