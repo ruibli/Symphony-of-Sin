9a8131d5771510bce2dce4f2a7df6e8fc3c2511b
ruibli <144478523+ruibli@users.noreply.github.com>
2024-04-15 10:14:00 -0400

    inv done

--- assets/gui/inventory.gd
+++ assets/gui/inventory.gd
@@ -5,10 +5,10 @@ var inv = []
 
 func _process(_delta):
 	inv = Glova.g_inv()
+	
 	if save != inv:
 		var temp = TextureRect.new()
 		temp.texture = load("res://assets/loot/items/" + str(inv[inv.size()-1]) + ".png")
-		temp.modulate = Color(1, 1, 1, 0.5)
 		temp.stretch_mode = 2
 		$inv_con.add_child(temp)
-		save = inv
+		save.append(str(inv[inv.size()-1]))
--- assets/gui/stats.gd
+++ assets/gui/stats.gd
@@ -6,7 +6,33 @@ var attack = 1
 var gold = 0
 
 func _process(_delta):
-	$VBoxContainer/speed_con/speed_label.text = str(Glova.g_stats()[2])
-	$VBoxContainer/power_con/power_label.text = str(Glova.g_stats()[3])
-	$VBoxContainer/attack_con/attack_label.text = str(Glova.g_stats()[4])
-	$VBoxContainer/gold_con/gold_label.text = str(Glova.g_stats()[5])
+	speed = snapped(Glova.g_stats()[2], 0.01)
+	power = snapped(Glova.g_stats()[3], 0.01)
+	attack = snapped(Glova.g_stats()[4], 0.01)
+	gold = str(snapped(Glova.g_stats()[5], 0.01))
+	
+	if speed - int(speed) == 0:
+		speed = str(speed) + ".00"
+	elif 10*speed - int(10*speed) == 0:
+		speed = str(speed) + "0"
+	else:
+		speed = str(speed)
+		
+	if power - int(power) == 0:
+		power = str(power) + ".00"
+	elif 10*power - int(10*power) == 0:
+		power = str(power) + "0"
+	else:
+		power = str(power)
+	
+	if attack - int(attack) == 0:
+		attack = str(attack) + ".00"
+	elif 10*attack - int(10*attack) == 0:
+		attack = str(attack) + "0"
+	else:
+		attack = str(attack)
+	
+	$VBoxContainer/speed_con/speed_label.text = speed
+	$VBoxContainer/power_con/power_label.text = power
+	$VBoxContainer/attack_con/attack_label.text = attack
+	$VBoxContainer/gold_con/gold_label.text = gold
