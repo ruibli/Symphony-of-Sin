a1a154acf33c762cf0b9c84c271548f6234991df
ruibli <144478523+ruibli@users.noreply.github.com>
2024-04-19 10:14:36 -0400

    pause menu almost done

--- assets/gui/pause.gd
+++ assets/gui/pause.gd
@@ -1,22 +1,72 @@
 extends Control
 
+var can_pause = true
 var level = 1
 var mod = 0
 
+var time =  0.0
+var hours = 0
+var minutes = 0
+var seconds = 0
+
 func _ready():
+	$fade/black/AnimationPlayer.play("clear")
 	hide()
 
-func _process(_delta):
-	if Input.is_action_pressed("pause"):
-		if get_tree().paused == true: # unpause
-			hide()
-			get_tree().paused = false
-		else: # pause
-			show()
-			get_tree().paused = true
-			level = Glova.g_level(0)
-			mod = Glova.g_mod(0)
+func _process(delta):
+	if Input.is_action_just_pressed("pause"):
+		if can_pause:
+			if get_tree().paused == true: # unpause
+				hide()
+				get_tree().paused = false
+				can_pause = false
+				$pause_timer.start()
+			else: # pause
+				show()
+				get_tree().paused = true
+				$main.grab_focus()
 	
-	# $VBoxContainer/level_con/level.text = str(level)
-	# $VBoxContainer/mod_con/mod.text = str(mod)
-	# $VBoxContainer/time_con/attack_time.text = str(time)
+	if get_tree().paused == false:
+		time += delta
+	
+	level = str(Glova.g_level(0))
+	mod = str(Glova.g_mod(0))
+	hours = int(fmod(time, 216000)/3600)
+	minutes = int(fmod(time, 3600)/60)
+	seconds = int(fmod(time, 60))
+
+	if hours < 10:
+		hours = "0" + str(hours) + ":"
+	else:
+		hours = str(hours) + ":"
+		
+	if minutes < 10:
+		minutes = "0" + str(minutes) + ":"
+	else:
+		minutes = str(minutes) + ":"
+		
+	if seconds < 10:
+		seconds = "0" + str(seconds)
+	else:
+		seconds = str(seconds)
+	
+	$VBoxContainer/level_con/level.text = level
+	$VBoxContainer/mod_con/mod.text = mod
+	$VBoxContainer/time_con/HBoxContainer/hours.text = hours
+	$VBoxContainer/time_con/HBoxContainer/minutes.text = minutes
+	$VBoxContainer/time_con/HBoxContainer/seconds.text = seconds
+
+func _on_timer_timeout():
+	can_pause = true
+
+func _on_main_pressed():
+	print("test")
+	$fade/black/AnimationPlayer.play("black")
+	await get_tree().create_timer(0.25).timeout
+	get_tree().change_scene_to_file("res://assets/gui/main.tscn")
+
+func _on_main_focus_entered():
+	$main.modulate = Color(190/255.0,118/255.0,253/255.0, 1)
+
+func _on_main_focus_exited():
+	$main.modulate = Color(1,1,1,1)
