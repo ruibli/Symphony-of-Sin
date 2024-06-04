3efc4af7b8c2b4cca7fa70cb13ba6358a8f46b83
ruibli <144478523+ruibli@users.noreply.github.com>
2024-06-03 23:07:44 -0400

    pride done

--- assets/entities/nova/nova.gd
+++ assets/entities/nova/nova.gd
@@ -243,13 +243,13 @@ func hit(ow,nam,dir):
 		if nam == "bite":
 			tween = get_tree().create_tween()
 			if dir == "up":
-				knockback = Vector2(0,-25)
+				knockback = Vector2(0,-10)
 			elif dir == "down":
-				knockback = Vector2(0,25)
+				knockback = Vector2(0,10)
 			elif dir == "left":
-				knockback = Vector2(-25,0)
+				knockback = Vector2(-10,0)
 			elif dir == "right":
-				knockback = Vector2(25,0)
+				knockback = Vector2(10,0)
 			tween.tween_property(self, "knockback", Vector2(0,0), 0.5)
 		
 		if Glova.stats[0] - ow < 1:
--- assets/entities/pride/blasthit.gd
+++ assets/entities/pride/blasthit.gd
@@ -1,26 +1,28 @@
 extends RayCast2D
 
-var damage = 50
+var velocity = Vector2(0,0)
+var damage = 40
 var dir
 var attack = 1
 
 func _ready():
-	$antlers.hide()
+	$Antlers.hide()
+	velocity = Vector2(0,0)	
 	$Timer.wait_time = $Timer.wait_time / attack
 	$Timer.start()
 
 func _process(_delta):
 	if is_colliding:
 		var cast = to_local(get_collision_point())
-		$antlers.points[1] = Vector2(0,cast.y-6)
+		$Antlers.points[1] = Vector2(0,cast.y-6)
 	else:
-		$antlers.points[1] = Vector2(0,320)
+		$Antlers.points[1] = Vector2(0,320)
 		
 	$antlershit.set_collision_mask_value(3,true)
-	$antlers.show()
+	$Antlers.show()
 
 func _on_timer_timeout():
 	queue_free()
 
-func _on_antletshit_area_entered(area):
+func _on_area_entered(area):
 	area.hit(damage,name,dir)
